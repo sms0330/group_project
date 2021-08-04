de60fc69f1efe09b2d1784d03629bb064fa45b65
Frannyngo <frannytaan@gmail.com>
2021-08-04 15:46:12 -0700

    student model

---
+++ db/migrate/20210804223659_create_students.rb
@@ -0,0 +1,11 @@
+class CreateStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :students do |t|
+      t.string :name 
+      t.string :email, unique:true
+      t.string :passphrase
+
+      t.timestamps
+    end
+  end
+end
