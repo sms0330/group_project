64685fa37097f82e35619b39ef7da87198956a87
Joseph Son <sms0330@gmail.com>
2021-08-05 15:21:32 -0700

    Added user authorization for course model

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,7 @@
 class CoursesController < ApplicationController
+    before_action :find_course_id, only: [:show, :edit, :update, :destroy]
     before_action :authenticate_user!, except: [:index, :show]
+    before_action :authorize_user!, only: [:edit, :update, :destroy]
 
     def home
     end
@@ -9,7 +11,6 @@ class CoursesController < ApplicationController
     end
 
     def show
-        @course = Course.find params[:id]
     end
 
     def new
@@ -17,7 +18,7 @@ class CoursesController < ApplicationController
     end
 
     def create 
-        @course = Course.new params.require(:course).permit(:title, :category, :description, :seats, :start_date, :end_date, :time)
+        @course = Course.new course_params
         @course.user = current_user
         if @course.save
             flash[:notice] = "Course created successfully!"
@@ -28,25 +29,34 @@ class CoursesController < ApplicationController
     end
 
     def edit
-        @course = Course.find params[:id]
     end
 
     def update
-        @course = Course.find params[:id]
-
-        if @course.update params.require(:course).permit(:title, :category, :description, :seats, :start_date, :end_date, :time)
+        if @course.update course_params
             flash[:notice] = 'Course has been updated!'
             redirect_to course_path(@course.id)
         else
-            flash[:notice] = 'Error!'
+            render :edit
         end
     end
 
     def destroy
-        @course = Course.find params[:id]
         @course.destroy
         flash[:notice] = 'Course has been deleted'
         redirect_to courses_path
     end
 
+    private
+
+    def course_params
+        params.require(:course).permit(:title, :category, :description, :seats, :start_date, :end_date, :time)
+    end
+    
+    def find_course_id
+        @course = Course.find params[:id]
+    end
+
+    def authorize_user!
+        redirect_to root_path, alert: 'Not authorized! please try again' unless can?(:crud, @course)
+    end
 end
