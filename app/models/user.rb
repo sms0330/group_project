98f4886a9ecf26dfbd264d1c6cec8c24d5155e2d
Chinmaya-Mathur <mathurchinmaya@gmail.com>
2021-08-06 01:18:06 +0530

    fixed sign in, sign_up and courses button

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
       has_many :courses, dependent: :destroy
       has_many :facilities, dependent: :nullify
 
-      validates :email, presence: true, uniqueness: { case_sensitive: false }, format: VALID_EMAIL_REGEX
+      has_secure_password
+
       validates :first_name, presence: true
       validates :last_name, presence: true
 
       VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+      validates :email, presence: true, uniqueness: { case_sensitive: false }, format: VALID_EMAIL_REGEX
      
       def full_name
             "#{first_name} #{last_name}".strip.titlecase
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
     root to: "courses#home"
 
     resources :courses
+    resources :users, only: [:new, :create]
+
+    resource :session, only: [:new, :create, :destroy]
 
 end
