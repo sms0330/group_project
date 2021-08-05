ee503de83c03f2d0e6a10ae86fe0998d7bb1e244
Frannyngo <frannytaan@gmail.com>
2021-08-05 14:10:47 -0700

    tested works

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     root to: "courses#home"
 
     resources :courses
+    resources :facilities
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_08_05_190347) do
+ActiveRecord::Schema.define(version: 2021_08_05_194314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "courses", force: :cascade do |t|
-    t.string "title"
+    t.string "name"
     t.string "category"
     t.text "description"
     t.integer "seats"
@@ -26,7 +25,11 @@ ActiveRecord::Schema.define(version: 2021_08_05_190347) do
     t.datetime "time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "teacher_id", null: false
+    t.bigint "student_id", null: false
     t.bigint "user_id"
+    t.index ["student_id"], name: "index_courses_on_student_id"
+    t.index ["teacher_id"], name: "index_courses_on_teacher_id"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
@@ -36,7 +39,38 @@ ActiveRecord::Schema.define(version: 2021_08_05_190347) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.string "description"
     t.index ["user_id"], name: "index_facilities_on_user_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "teacher_id", null: false
+    t.bigint "course_id", null: false
+    t.index ["course_id"], name: "index_students_on_course_id"
+    t.index ["email"], name: "index_students_on_email"
+    t.index ["teacher_id"], name: "index_students_on_teacher_id"
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "course_id", null: false
+    t.bigint "student_id", null: false
+    t.bigint "facility_id", null: false
+    t.index ["course_id"], name: "index_teachers_on_course_id"
+    t.index ["email"], name: "index_teachers_on_email"
+    t.index ["facility_id"], name: "index_teachers_on_facility_id"
+    t.index ["student_id"], name: "index_teachers_on_student_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,6 +85,12 @@ ActiveRecord::Schema.define(version: 2021_08_05_190347) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "courses", "students"
+  add_foreign_key "courses", "teachers"
   add_foreign_key "courses", "users"
   add_foreign_key "facilities", "users"
+  add_foreign_key "students", "courses"
+  add_foreign_key "students", "teachers"
+  add_foreign_key "teachers", "courses"
+  add_foreign_key "teachers", "students"
 end
