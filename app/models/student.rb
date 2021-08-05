8e73ecaffe62366836a457bec4a33b0657fbe1ed
Frannyngo <frannytaan@gmail.com>
2021-08-04 21:53:13 -0700

    courses final

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,15 @@
 class Student < ApplicationRecord
+    
+    has_many :teachers, dependent: :nullify
 
-    validates :name, presence:true
-    validates :email, presence:true
-    validates :passphrase, presence:true 
+    has_many :courses, dependent: :destroy
+    has_many :coursed_teachers, through: :courses, source: :teacher
 
+    validates :email, presence: true, uniqueness: { case_sensitive: false }
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    
+    def full_name
+        "#{first_name} #{last_name}".strip.titlecase
+    end
 end
