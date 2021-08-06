230e27529973ca6b701053cd57f559bb478f782c
Schen0001 <chen.steven609@gmail.com>
2021-08-05 17:30:59 -0700

    Added radio buttons for sign up page. Specifically tweaked the migration file for :users table and also /user/new.html.erb file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_190347) do
+ActiveRecord::Schema.define(version: 2021_08_05_194314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,26 +28,28 @@ ActiveRecord::Schema.define(version: 2021_08_05_190347) do
     t.bigint "user_id"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
+
   create_table "facilities", force: :cascade do |t|
     t.string "name"
     t.boolean "book"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "description"
     t.bigint "user_id"
+    t.string "description"
     t.index ["user_id"], name: "index_facilities_on_user_id"
   end
+
   create_table "users", force: :cascade do |t|
+    t.boolean "is_teacher"
     t.string "first_name"
     t.string "last_name"
     t.string "email"
     t.string "password_digest"
-    t.boolean "is_teacher"
-    t.boolean "is_student"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
   add_foreign_key "courses", "users"
   add_foreign_key "facilities", "users"
 end
