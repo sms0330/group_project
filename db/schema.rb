c2e51fc315d89578a69efa6ced13c2e6fc6acc9a
Schen0001 <chen.steven609@gmail.com>
2021-08-06 01:47:01 -0700

    styled courses show page

--- db/schema.rb
+++ db/schema.rb
@@ -1,7 +1,18 @@
-
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_08_05_232627) do
 
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "courses", force: :cascade do |t|
@@ -9,16 +20,12 @@ ActiveRecord::Schema.define(version: 2021_08_05_232627) do
     t.string "category"
     t.text "description"
     t.integer "seats"
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.datetime "time"
+    t.date "start_date"
+    t.date "end_date"
+    t.time "time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "teacher_id", null: false
-    t.bigint "student_id", null: false
-    t.bigint "user_id"
-    t.index ["student_id"], name: "index_courses_on_student_id"
-    t.index ["teacher_id"], name: "index_courses_on_teacher_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
@@ -27,18 +34,17 @@ ActiveRecord::Schema.define(version: 2021_08_05_232627) do
     t.boolean "book"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.string "description"
     t.index ["user_id"], name: "index_facilities_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "is_teacher"
     t.string "first_name"
     t.string "last_name"
     t.string "email"
     t.string "password_digest"
-    t.boolean "is_teacher"
-    t.boolean "is_student"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "is_admin"
