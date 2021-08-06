2d4e5b5770505820b40e58adaf4839c007d81713
Joseph Son <sms0330@gmail.com>
2021-08-05 17:26:45 -0700

    Added admin user and fake seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_190347) do
+ActiveRecord::Schema.define(version: 2021_08_05_232627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,15 +28,17 @@ ActiveRecord::Schema.define(version: 2021_08_05_190347) do
     t.bigint "user_id"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
+
   create_table "facilities", force: :cascade do |t|
     t.string "name"
     t.boolean "book"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "description"
     t.bigint "user_id"
+    t.string "description"
     t.index ["user_id"], name: "index_facilities_on_user_id"
   end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -46,8 +48,10 @@ ActiveRecord::Schema.define(version: 2021_08_05_190347) do
     t.boolean "is_student"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
   add_foreign_key "courses", "users"
   add_foreign_key "facilities", "users"
 end
